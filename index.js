244d6ed0ff0fac4fc2ad9730e1b39c8c7133bfab
zesciati <zakyabiyyu2003@gmail.com>
2025-07-14 12:48:54 +0800

    proses sampai ke Reassigning dan Incrementing

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 // document mengacu ke file html
 // id dari h2
 // innerText akan menampilkan value h2 yg di set
-document.getElementById("count-el").innerText = 5
+// document.getElementById("count-el").innerText = 5
+
+/* ----------------- // NOTE - Basic mathematical operations ---------------- */
+// 1. Create two variables, myAge and humanDogRatio
+// 2. Multiply the two together and store the result in myDogAge 
+// 3. Log myDogAge to the console
+
+// let myAge = 21;
+// let humanDogRation = 7;
+// let myDogAge = myAge * humanDogRation;
+// console.log(myDogAge);
+
+
+/* -------------------------------------------------------------------------- */
+/*                   // NOTE - Reassigning and incrementing                   */
+/* -------------------------------------------------------------------------- */
+let bonusPoints = 50;
+let increaseBonusPoints = bonusPoints +  50;
+console.log(increaseBonusPoints);
+let decreaseBonusPoints = increaseBonusPoints - 75;
+console.log(decreaseBonusPoints);
+let increaseAgainBonusPoints = decreaseBonusPoints + 45;
+console.log(increaseAgainBonusPoints);
+  
